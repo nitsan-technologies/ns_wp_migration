90d252b91fc69970bab92a9a86f005c30760d9a7
Navdeep-Nitsan <navdeep.nitsan@gmail.com>
2024-03-27 20:44:52 +0530

    [TASK] Code improvements

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF['ns_wp_migration'] = [
     'title' => '[Nitsan] WP Migration',
     'description' => 'Plug-n-play TYPO3 extension to migrate wordpress post and page data to your TYPO3 site. This extension includes features like Migrate post to EXT:news or EXT:blog, migrate categories, migrate tags, migrate media files etc.',
     'category' => 'be',
-    'author' => 'Navdeepsinh Jethwa',
+    'author' => 'T3: Navdeepsinh Jethwa',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
@@ -15,7 +15,6 @@ $EM_CONF['ns_wp_migration'] = [
             'news' => '11.0.0-11.4.99',
             'blog' => '11.0.2-12.0.2',
             'md_news_author' => '7.0.0-7.0.2',
-            'ns_basetheme'=> '11.5.8-12.5.10'
         ],
         'conflicts' => [],
         'suggests' => [],
