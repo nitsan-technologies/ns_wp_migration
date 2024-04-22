eda7c27d1fe8932f3ef59eb0c87cc2238d23daab
nitsan-technologies <nitsan-technologies>
2024-04-22 16:08:25 +0530

    [TASK] Description Updated

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF['ns_wp_migration'] = [
     'title' => '[Nitsan] WP Migration',
-    'description' => 'Plug-n-play TYPO3 extension to migrate wordpress post and page data to your TYPO3 site. This extension includes features like Migrate post to EXT:news or EXT:blog, migrate categories, migrate tags, migrate media files etc.',
+    'description' => 'Plug-n-play TYPO3 extension to migrate wordpress page and page data to your TYPO3 site.',
     'category' => 'be',
     'author' => 'T3: Navdeepsinh Jethwa',
     'author_email' => 'sanjay@nitsan.in',
